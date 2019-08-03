322c69e5023b49ab76f85d05c0f9fb3903e2af8d
Flor <florivonne.bg@gmail.com>
2019-08-03 11:50:22 -0500

    add vuetify

---
+++ ohm/src/components/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div id="content">
+            <h3>Sample h3 tag</h3>
+            <p>Sample pararaph</p>
+        </div>
+<button @click="downloadPDF">Descargar</button>
+    </div>
+</template>
+
+<script>
+import jsPDF from 'jspdf';
+
+export default {
+    methods: {
+        downloadPDF() {
+            let generate = new jsPDF();
+            let source = document.getElementById('content')
+            generate.fromHTML(source, 15, 15)
+            generate.save('sample-file.pdf')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
